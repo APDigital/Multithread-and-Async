e8d934c0bf82b7bb99d2b237f90a1157e8e43257
APDigital <aoifejo.pearson@googlemail.com>
2018-01-30 12:13:02 +0000

    Deadlock with multiple instances of DataStore

---
+++ Multithreaded and Asynchronous/DataStore.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreaded_and_Asynchronous
+{
+    class DataStores
+    {
+        private class DataStore { public int Value { get; set; } }
+
+        private DataStore firstStore = new DataStore();
+        private DataStore secondStore = new DataStore();
+
+        public void ConcurrencyTest()
+        {
+            var thread1 = new Thread(IncrementTheValue);
+            var thread2 = new Thread(IncrementTheValue);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join(); // Wait for the thread to finish executing
+            thread2.Join();
+
+            Console.WriteLine($"Final values: First:{firstStore.Value}, Second:{secondStore.Value}");
+        }
+
+        private void IncrementTheValue()
+        {
+            lock (firstStore)
+            {
+                lock (secondStore)
+                {
+                    firstStore.Value++;
+                    secondStore.Value++;
+                }
+                
+            }
+        }
+    }
+}
