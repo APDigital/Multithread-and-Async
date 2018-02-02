281912872d0a0853dba085605e6b64df784a1902
APDigital <aoifejo.pearson@googlemail.com>
2018-02-02 12:06:43 +0000

    Async

--- Multithreaded and Asynchronous/CountNonExistent.cs
+++ Multithreaded and Asynchronous/CountNonExistent.cs
@@ -14,11 +14,10 @@ namespace Multithreaded_and_Asynchronous
             Task<string> articleTask = new WebClient().DownloadStringTaskAsync(@"https://msdn.microsoft.com/en-gb/library/mt674882.aspx");
             Task<string> wordsTask = new WebClient().DownloadStringTaskAsync(@"https://github.com/dwyl/english-words");
 
-            string article = await articleTask;
             string words = await wordsTask;
-
             HashSet<string> wordList = new HashSet<string>(words.Split('\n'));
-
+            string article = await articleTask;
+            
             var nonExistentWords = 0;
 
             foreach (string word in article.Split('\n', ' '))
